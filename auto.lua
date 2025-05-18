37c9079e7d807b6759c2061547c582a627580e8b
wedfwef3 <eeeiqjjj239@gmail.com>
2025-05-17 23:50:37 -0400

    Update auto.lua

--- auto.lua
+++ auto.lua
@@ -1,41 +1,38 @@
 task.spawn(function()
-    local duration = 15 -- Total time to run (seconds)
-    local interval = 3 -- How often to run (seconds)
-    local startTime = tick() -- Get the current time
+    local duration = 15
+    local interval = 3
+    local startTime = tick()
 
     while tick() - startTime < duration do
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/fly.github.io/refs/heads/main/fly.lua"))()
-        task.wait(interval) -- Wait before running again
+        task.wait(interval)
     end
 end)
-
-local workspace = game:GetService("Workspace")
-local vampireCastle = workspace:FindFirstChild("VampireCastle")
 
 local workspace = game:GetService("Workspace")
 local vampireCastle = workspace:FindFirstChild("VampireCastle")
 
 if vampireCastle then
-    while true do
-        local bookcase = vampireCastle:FindFirstChild("Bookcase")
-        if bookcase then
-            bookcase:Destroy()
-            print("Bookcase removed!")
+    task.spawn(function() -- Use task.spawn to prevent blocking
+        while true do
+            local bookcase = vampireCastle:FindFirstChild("Bookcase")
+            if bookcase then
+                bookcase:Destroy()
+                print("Bookcase removed!")
+            end
+            task.wait(0.1)
         end
-        task.wait(0.1) -- Keeps checking every 0.1 seconds
-    end
+    end)
 else
-    print("No VampireCastle found in Workspace
-
+    print("No VampireCastle found in Workspace.") -- Fix syntax error
+end
 
 task.spawn(function()
-    task.wait(8) -- ring
+    task.wait(8)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/castletpfast.github.io/refs/heads/main/FASTCASTLE.lua"))()
 end)
 
-
 task.spawn(function()
-    task.wait(12) -- ring
+    task.wait(12)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/wedfwef3/wefwef.github.io/refs/heads/main/erf.lua"))()
 end)
-
