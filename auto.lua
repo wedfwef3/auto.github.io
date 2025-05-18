fcc3e07abca33425fcc638f1bcea3f18f28484cd
wedfwef3 <eeeiqjjj239@gmail.com>
2025-05-17 20:02:28 -0400

    Create auto.lua

---
+++ auto.lua
@@ -0,0 +1,10 @@
+task.spawn(function()
+    task.wait(8) -- ring
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/castletpfast.github.io/refs/heads/main/FASTCASTLE.lua"))()
+end)
+
+
+task.spawn(function()
+    task.wait(12) -- ring
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wedfwef3/wefwef.github.io/refs/heads/main/erf.lua"))()
+end)
